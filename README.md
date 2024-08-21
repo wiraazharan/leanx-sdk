11e5561845e2b95eabe4c140266d88e3f2567401
wiraazharanakmar <wiraazharan@gmail.com>
2024-08-21 14:51:59 +0800

    Enhancement on SDK

--- README.md
+++ README.md
@@ -4,12 +4,11 @@
 
 <!-- [![Latest Version](https://img.shields.io/github/release/wiraazharan/leanx-sdk.svg?style=flat-square)](https://github.com/wiraazharan/leanx-sdk/releases)
 [![Build Status](https://img.shields.io/github/actions/workflow/status/wiraazharan/leanx-sdk/ci.yml?label=ci%20build&style=flat-square)](https://github.com/wiraazharan/leanx-sdk/actions?query=workflow%3ACI) -->
-[![Total Downloads](https://img.shields.io/packagist/dt/wiraazharan/leanx-sdk.svg?style=flat-square)](https://packagist.org/packages/wiraazharan/leanx-sdk)
+<!-- [![Total Downloads](https://img.shields.io/packagist/dt/wiraazharan/leanx-sdk.svg?style=flat-square)](https://packagist.org/packages/wiraazharan/leanx-sdk) -->
 
 LeanxSdk is a PHP SDK designed to simplify integration with the LeanX payment API. This SDK provides easy-to-use methods for generating payment links, validating payments, and checking payment statuses.
 
 - Easy setup and usage for LeanX payment integration.
-- Supports both synchronous and asynchronous requests.
 - Flexible configuration with dynamic base URLs and endpoints.
 - Comprehensive support for generating and managing payments.
 
@@ -18,21 +17,24 @@ LeanxSdk is a PHP SDK designed to simplify integration with the LeanX payment AP
 To get started with `LeanxSdk`, follow these steps:
 
 ### Instantiate the SDK
-
 ```php
 use Wiraazharan\LeanxSdk\LeanxSdk;
 
-// Replace 'your-auth-token' with your actual LeanX auth token
-$authToken = 'your-auth-token';
 // Base URL for LeanX API (use sandbox or live URL as needed)
-$baseUrl = 'https://stag-api.leanpay.my';
+$baseUrl = 'https://api.leanx.dev';
+// Replace 'your-auth-token' with your actual LeanX auth token
+$authToken = 'LP-922AFF8C-MM|cdffb385-7f8b-4fa8-9bdf-5563c7b6c2cb|a59592d0bf0620ac09ee406b91f7006b59b96a35d1406dc3101e383dfa9abf820686064e7b281adc71a3a3a36eed5xxxxxxxxxxxxe41ad25a0d07axxxxx';
+// Replace 'hashKey' with your actual LeanX Hash Key
+$hashKey = 'a59592d0bf0620ac09ee406b91f7006b59b96a35d1406dc3101e383dfa9abf820686064e7b281adc71a3a3xxxxxxxe1ae77b39e41ad25a0d07a2525531';
+// Replace 'UUID' with your actual LeanX UUID
+$UUID = 'cdffb385-7f8b-xxxxxxx-c7b6c2cb';
 
 // Create a new instance of the SDK
-$sdk = new LeanxSdk($authToken, $baseUrl);
+$sdk = new LeanxSdk($baseUrl, $authToken, $hashKey, $UUID);
 ```
 
 ### Generate a Payment Link
-### To generate a payment link, use the generatePaymentLink method. Here is an example of how to use it:
+### To generate a payment link, use the GeneratePaymentLinkv1 method. Here is an example of how to use it:
 ``` php
 $parameters = [
     'amount' => 100.00,
@@ -44,25 +46,48 @@ $parameters = [
     'callback_url' => 'https://yourwebsite.com/callback'
 ];
 
-$response = $sdk->generatePaymentLink($parameters);
+$response = $sdk->GeneratePaymentLinkv1($parameters);
 print_r($response);
 ```
 
-### Validate a Payment
-```php
+### Generate a HMAC Payment Link
+### To generate a HMAC payment link, use the GeneratePaymentLinkv1WithHashmac method. Here is an example of how to use it:
+``` php
 $parameters = [
-    'invoice_no' => 'INV12345'
+    'amount' => 100.00,
+    'full_name' => 'John Doe',
+    'email' => 'john.doe@example.com',
+    'phone_number' => '0123456789',
+    'invoice_no' => 'INV12345',
+    'redirect_url' => 'https://yourwebsite.com/redirect',
+    'callback_url' => 'https://yourwebsite.com/callback'
 ];
 
-$response = $sdk->validatePayment($parameters);
+$response = $sdk->GeneratePaymentLinkv1WithHashmac($parameters);
 print_r($response);
 ```
 
+### Generate Auto Bill Page
+### To generate Auto Bill Page, use the CreateAutoBillPagev1WithHashmac method. Here is an example of how to use it:
+``` php
+$parameters = [
+    'amount' => 100.00,
+    'full_name' => 'John Doe',
+    'email' => 'john.doe@example.com',
+    'phone_number' => '0123456789',
+    'invoice_no' => 'INV12345',
+    'redirect_url' => 'https://yourwebsite.com/redirect',
+    'callback_url' => 'https://yourwebsite.com/callback'
+];
+
+$response = $sdk->CreateAutoBillPagev1WithHashmac($parameters);
+print_r($response);
+```
 
 ### Check Payment Status
 ```php
 $invoiceNo = 'INV12345';
 
-$response = $sdk->checkPaymentStatus($invoiceNo);
+$response = $sdk->CheckPaymentStatusV1WithHmac($invoiceNo);
 print_r($response);
 ```
--- src/LeanxSdk.php
+++ src/LeanxSdk.php
@@ -2,46 +2,153 @@
 
 namespace Wiraazharan\LeanxSdk;
 
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Exception\RequestException;
+use stdClass;
 
 class LeanxSdk
 {
     protected $client;
     protected $authToken;
     protected $baseUrl;
+    protected $hashKey;
+    protected $UUID;
 
-    public function __construct(string $authToken, string $baseUrl)
+    public function __construct(string $baseUrl, string $authToken, string $hashKey, string $UUID)
     {
         $this->authToken = $authToken;
+        $this->hashKey = $hashKey;
+        $this->UUID = $UUID;
         $this->baseUrl = rtrim($baseUrl, '/');
         $this->client = new Client();
     }
 
-    public function generatePaymentLink(array $parameters)
+    //NON HMAC ENDPOINTS;
+    public function GeneratePaymentLinkv1(array $parameters)
     {
-        $endpoint = '/merchant/create-bill-page';
-        return $this->sendRequest('POST', $endpoint, $parameters);
+        $URLPATH = "/api/v1/merchant/create-bill-page";
+        $HTTPMETHOD = "POST";
+        $endpoint = $this->baseUrl . $URLPATH;
+
+        $header = [
+            'Content-Type' => 'application/json',
+            'auth-token' => $this->authToken,
+        ];
+
+        $body = $parameters;
+        $response = json_decode($this->PostRequestMamJsonBody($endpoint, $header, $body));
+        $responses = [
+            "url" => $endpoint,
+            "header" => $header,
+            "body" => $body,
+            "response" => $response,
+        ];
+        return $responses;
+    }
+    //NON HMAC ENDPOINTS;
+
+    //HMAC ENDPOINTS;
+    public function GeneratePaymentLinkv1WithHashmac(array $parameters)
+    {
+        $URLPATHFORSIGNATURE = "/api/v1/merchant/create-bill-page";
+        $URLPATH = "/api/v1/merchant/create-bill-page";
+        $HTTPMETHOD = "POST";
+        $endpoint = $this->baseUrl . $URLPATH;
+
+        //GENERATE SIGNATURE
+        $signatures = $this->GenerateSignature($this->UUID, $this->authToken, $this->hashKey, $HTTPMETHOD, $URLPATHFORSIGNATURE);
+        //GENERATE SIGNATURE
+
+        $header = [
+            'Content-Type' => 'application/json',
+            'auth-token' => $this->authToken,
+            'x-signature' => $signatures["x-signature"],
+            'x-timestamp' => $signatures["x-timestamp"],
+            'x-nonce' => $signatures["x-nonce"],
+        ];
+
+        $body = $parameters;
+
+        $response = json_decode($this->PostRequestMamJsonBody($endpoint, $header, $body));
+        $responses = [
+            "url" => $endpoint,
+            "signature" => $signatures,
+            "header" => $header,
+            "body" => $body,
+            "response" => $response,
+        ];
+        return $responses;
     }
 
-    public function validatePayment(array $parameters)
+    public function CreateAutoBillPagev1WithHashmac(array $parameters)
     {
-        $endpoint = '/merchant/validate';
-        return $this->sendRequest('POST', $endpoint, $parameters);
+        $URLPATHFORSIGNATURE = "/api/v1/merchant/create-auto-bill-page";
+        $URLPATH = "/api/v1/merchant/create-auto-bill-page";
+        $HTTPMETHOD = "POST";
+        $endpoint = $this->baseUrl . $URLPATH;
+
+        //GENERATE SIGNATURE
+        $signatures = $this->GenerateSignature($this->UUID, $this->authToken, $this->hashKey, $HTTPMETHOD, $URLPATHFORSIGNATURE);
+        //GENERATE SIGNATURE
+
+        $header = [
+            'Content-Type' => 'application/json',
+            'auth-token' => $this->authToken,
+            'x-signature' => $signatures["x-signature"],
+            'x-timestamp' => $signatures["x-timestamp"],
+            'x-nonce' => $signatures["x-nonce"],
+        ];
+
+        $body = $parameters;
+
+        $response = json_decode($this->PostRequestMamJsonBody($endpoint, $header, $body));
+        $responses = [
+            "url" => $endpoint,
+            "signature" => $signatures,
+            "header" => $header,
+            "body" => $body,
+            "response" => $response,
+        ];
+        return $responses;
     }
 
-    public function checkPaymentStatus(string $invoiceNo)
+    public function CheckPaymentStatusV1WithHmac(string $invoiceNo)
     {
-        $endpoint = '/merchant/manual-checking-transaction';
-        return $this->sendRequest('GET', $endpoint, ['invoice_no' => $invoiceNo]);
-    }
+        $URLPATHFORSIGNATURE = "/api/v1/merchant/manual-checking-transaction";
+        $URLPATH = "/api/v1/merchant/manual-checking-transaction?invoice_no=" . $invoiceNo;
+        $HTTPMETHOD = "POST";
+        $endpoint = $this->baseUrl . $URLPATH;
 
-    public function createBillPage(array $parameters)
-    {
-        $endpoint = '/merchant/create-bill-page';
-        return $this->sendRequest('POST', $endpoint, $parameters, true);
+        //GENERATE SIGNATURE
+        $signatures = $this->GenerateSignature($this->UUID, $this->authToken, $this->hashKey, $HTTPMETHOD, $URLPATHFORSIGNATURE);
+        //GENERATE SIGNATURE
+
+        $header = [
+            'Content-Type' => 'application/json',
+            'auth-token' => $this->authToken,
+            'x-signature' => $signatures["x-signature"],
+            'x-timestamp' => $signatures["x-timestamp"],
+            'x-nonce' => $signatures["x-nonce"],
+        ];
+
+        $body = [];
+        $response = json_decode($this->PostRequestLeanx($endpoint, $header, $body));
+        $responses = [
+            "url" => $endpoint,
+            "signature" => $signatures,
+            "header" => $header,
+            "body" => $body,
+            "response" => $response,
+        ];
+        return $responses;
     }
+    //HMAC ENDPOINTS;
+
+
+
+    //TOOLBOX................................................................
 
     protected function sendRequest(string $method, string $endpoint, array $parameters = [], bool $json = false)
     {
@@ -54,7 +161,7 @@ class LeanxSdk
 
         if ($json) {
             $headers['Content-Type'] = 'application/json';
-            $body = json_encode($parameters);
+            $body = $parameters;
         } else {
             $body = http_build_query($parameters);
         }
@@ -67,5 +174,356 @@ class LeanxSdk
         } catch (RequestException $e) {
             return ['error' => $e->getMessage()];
         }
+    }
+
+    protected static function PostRequestMamPlainText($endpoint, $header = null, $body)
+    {
+
+        try {
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $endpoint,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => $body,
+                CURLOPT_HTTPHEADER => $header,
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+
+            return $response;
+        } catch (Exception $e) {
+            // $obj = new stdClass;
+            // $obj->respDesc = Psr7\str($e->getRequest());
+            // return json_encode($obj);
+            return $e->getMessage();
+        }
+    }
+
+    protected static function PostRequestMam($endpoint, $header = null, $body)
+    {
+
+        try {
+            if ($header == null) {
+                $client = new Client([
+                    'headers' => [
+                        // 'Content-Type' => 'application/json',
+                        'Content-Type' => 'application/x-www-form-urlencoded',
+                    ],
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            } else {
+                $client = new Client([
+                    'headers' => $header,
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            }
+
+            $response = $client->post(
+                $endpoint,
+                [
+                    'form_params' => $body,
+                    'timeout' => env('REQUEST_TIMEOUT_IN_SECONDS', 3000),
+                ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (RequestException $e) {
+            // $obj = new stdClass;
+            // $obj->respDesc = Psr7\str($e->getRequest());
+            // return json_encode($obj);
+            if ($e->hasResponse()) {
+                $obj = new stdClass;
+                $exception = (string) $e->getResponse()->getBody();
+                $exception = json_decode($exception);
+                $obj->statusCode = $e->getCode();
+                $obj->respDesc = $exception;
+                return json_encode($obj);
+            } else {
+                $obj = new stdClass;
+                $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+                // "Connection Error. Check VPN and Etc.." ;
+                return json_encode($obj);
+            }
+        }
+    }
+
+    protected static function PostRequestMamJsonBody($endpoint, $header = null, $body)
+    {
+
+        try {
+            if ($header == null) {
+                $client = new Client([
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        // 'Content-Type' => 'application/x-www-form-urlencoded',
+                    ],
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            } else {
+                $client = new Client([
+                    'headers' => $header,
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            }
+
+            $response = $client->post(
+                $endpoint,
+                [
+                    'json' => $body,
+                    'timeout' => env('REQUEST_TIMEOUT_IN_SECONDS', 3000),
+                ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (RequestException $e) {
+            // $obj = new stdClass;
+            // $obj->respDesc = Psr7\str($e->getRequest());
+            // return json_encode($obj);
+            if ($e->hasResponse()) {
+                $obj = new stdClass;
+                $exception = (string) $e->getResponse()->getBody();
+                $exception = json_decode($exception);
+                $obj->statusCode = $e->getCode();
+                $obj->respDesc = $exception;
+                return json_encode($obj);
+            } else {
+                $obj = new stdClass;
+                $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+                // "Connection Error. Check VPN and Etc.." ;
+                return json_encode($obj);
+            }
+        }
+    }
+
+    protected static function PutRequestMamJsonBody($endpoint, $header = null, $body)
+    {
+
+        try {
+            if ($header == null) {
+                $client = new Client([
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        // 'Content-Type' => 'application/x-www-form-urlencoded',
+                    ],
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            } else {
+                $client = new Client([
+                    'headers' => $header,
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            }
+
+            $response = $client->put(
+                $endpoint,
+                [
+                    'json' => $body,
+                    'timeout' => env('REQUEST_TIMEOUT_IN_SECONDS', 3000),
+                ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (RequestException $e) {
+            // $obj = new stdClass;
+            // $obj->respDesc = Psr7\str($e->getRequest());
+            // return json_encode($obj);
+            if ($e->hasResponse()) {
+                $obj = new stdClass;
+                $exception = (string) $e->getResponse()->getBody();
+                $exception = json_decode($exception);
+                $obj->statusCode = $e->getCode();
+                $obj->respDesc = $exception;
+                return json_encode($obj);
+            } else {
+                $obj = new stdClass;
+                $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+                // "Connection Error. Check VPN and Etc.." ;
+                return json_encode($obj);
+            }
+        }
+    }
+
+    protected static function PostRequestMamMultipart($endpoint, $header = null, $body, $fileParameterName)
+    {
+        try {
+            if ($header == null) {
+                $client = new Client([
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                    ],
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            } else {
+                $client = new Client([
+                    'headers' => $header,
+                    'verify' => env('SSL_VERIFICATION', false)
+                ]);
+            }
+
+            // Check if body contains a file parameter
+            $hasFile = false;
+            foreach ($body as $key => $value) {
+                if ($value instanceof \Illuminate\Http\UploadedFile) {
+                    $hasFile = true;
+                    break;
+                }
+            }
+
+            $multipartBody = [];
+            $multipartBody[] = [
+                'name' => "file",
+                'contents' => fopen($body->{$fileParameterName}->path(), 'r'),
+                'filename' => $body->{$fileParameterName}->getClientOriginalName()
+            ];
+            // dd($multipartBody);
+            // foreach ($body as $key => $value) {
+            //     if ($value instanceof \Illuminate\Http\UploadedFile) {
+            //         $multipartBody[] = [
+            //             'name' => "file",
+            //             'contents' => fopen($value->path(), 'r'),
+            //             'filename' => $value->getClientOriginalName()
+            //         ];
+            //     } else {
+            //         $multipartBody[] = [
+            //             'name' => $key,
+            //             'contents' => $value
+            //         ];
+            //     }
+            // }
+
+            $response = $client->post(
+                $endpoint,
+                [
+                    'multipart' => $multipartBody,
+                    'timeout' => 300,
+                ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (RequestException $e) {
+            if ($e->hasResponse()) {
+                $obj = new stdClass;
+                $exception = (string) $e->getResponse()->getBody();
+                $exception = json_decode($exception);
+                $obj->statusCode = $e->getCode();
+                $obj->respDesc = $exception;
+                return json_encode($obj);
+            } else {
+                $obj = new stdClass;
+                $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+                return json_encode($obj);
+            }
+        }
+    }
+
+    protected static function PostRequestLeanx($endpoint, $header = null, $body)
+    {
+
+        try {
+            if ($header == null) {
+                $client = new Client([
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        // 'Content-Type' => 'application/x-www-form-urlencoded',
+                    ],
+                ]);
+            } else {
+                $client = new Client([
+                    'headers' => $header,
+                ]);
+            }
+
+            $response = $client->post(
+                $endpoint,
+                [
+                    // 'form_params' => $body,
+                    'json' => $body,
+                    'timeout' => env('REQUEST_TIMEOUT_IN_SECONDS', 3000),
+                ]
+            );
+
+            return $response->getBody()->getContents();
+        } catch (RequestException $e) {
+            // $obj = new stdClass;
+            // $obj->respDesc = Psr7\str($e->getRequest());
+            // return json_encode($obj);
+            if ($e->hasResponse()) {
+                $obj = new stdClass;
+                $exception = (string) $e->getResponse()->getBody();
+                $exception = json_decode($exception);
+                $obj->statusCode = $e->getCode();
+                $obj->respDesc = $exception;
+                $obj->response_code = 10000;
+                return json_encode($obj);
+            } else {
+                $obj = new stdClass;
+                $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+                $obj->response_code = 10000;
+                // "Connection Error. Check VPN and Etc.." ;
+                return json_encode($obj);
+            }
+        } catch (\GuzzleHttp\Exception\ConnectException $e) {
+            $obj = new stdClass;
+            $obj->respDesc = $e->getMessage() != null ? $e->getMessage() : "Connection Error. Check VPN and Etc..";
+            $obj->response_code = 10000;
+            // "Connection Error. Check VPN and Etc.." ;
+            return json_encode($obj);
+        }
+    }
+
+    protected static function GenerateSignature($UUID, $AUTH_TOKEN, $HASH_KEY, $HTTP_METHOD, $URL_PATH)
+    {
+        // Replace placeholders with actual values
+        $PLATFORM = "api";
+        $API_VERSION = "v1";
+
+        // Replace placeholders in the URL path
+        $URL_PATH = str_replace("{{PLATFORM}}", $PLATFORM, $URL_PATH);
+        $URL_PATH = str_replace("{{API_VERSION}}", $API_VERSION, $URL_PATH);
+
+        // Get the current timestamp in seconds
+        $TIME_STAMP = time();
+
+        // Generate a random UUID for the nonce
+        $NONCE = self::generateUuidV4();
+
+        // Create the message to be hashed
+        $message = "{$HTTP_METHOD}|{$UUID}|{$URL_PATH}|{$TIME_STAMP}|{$AUTH_TOKEN}|{$NONCE}";
+
+        // Generate the HMAC signature using SHA256
+        $hmacSignature = hash_hmac('sha256', $message, $HASH_KEY);
+
+        // Return the headers
+        return [
+            'x-signature' => $hmacSignature,
+            'x-timestamp' => $TIME_STAMP,
+            'x-nonce' => $NONCE
+        ];
+    }
+
+    private static function generateUuidV4()
+    {
+        return sprintf(
+            '%04x%04x-%04x-4%03x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
     }
 }
